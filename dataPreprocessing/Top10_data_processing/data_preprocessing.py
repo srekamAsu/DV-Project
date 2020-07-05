fec237da414d4c76d6c0e453a63347ca0d66d1a6
Shanchuan <sshao7@asu.edu>
2020-07-05 15:19:42 -0700

    top10 data processing for All_beauty categroy

--- dataPreprocessing/Top10_data_processing/data_preprocessing.py
+++ dataPreprocessing/Top10_data_processing/data_preprocessing.py
@@ -60,19 +60,19 @@ def load_ratings_data(ratings_csv_path):
 
 
 # monthly top10 avg_ratings
-def top10_avg_ratings_data(monthly_dic, meta_data, output_json_path, fashion_bad_asin):
+def top10_avg_ratings_data(monthly_dic, meta_data, output_json_path, bad_asin):
     top10_ratings_results = {}
     for i in range(1, 13):
         asin_rating_list = []
         sub_dic = monthly_dic[i]
         for asin in sub_dic:
-            if (sub_dic[asin][1] >= 15) and (asin not in fashion_bad_asin):
+            if (sub_dic[asin][1] >= 15) and (asin not in bad_asin):
                 asin_rating_list.append([asin, sub_dic[asin][0] / float(sub_dic[asin][1]), sub_dic[asin][1]])
         asin_rating_list = sorted(asin_rating_list, key=lambda x: x[1])[::-1][:10]
         sub_res = {}
         for item in asin_rating_list:
             # print (item[0])
-            #print(meta_data[item[0]])
+            # print(meta_data[item[0]])
             sub_res[item[0]] = {'title': meta_data[item[0]]['title'], 'avg_ratings': item[1], 'num_ratings': item[2],
                                 'brand': meta_data[item[0]]['brand'],
                                 'sub_categories': meta_data[item[0]]['sub_categories']}
@@ -83,13 +83,13 @@ def top10_avg_ratings_data(monthly_dic, meta_data, output_json_path, fashion_bad
 
 
 # monthly top10 most_ratings
-def top10_most_ratings_data(monthly_dic, meta_data, output_json_path, fashion_bad_asin):
+def top10_most_ratings_data(monthly_dic, meta_data, output_json_path, bad_asin):
     most10_ratings_results = {}
     for i in range(1, 13):
         asin_rating_list = []
         sub_dic = monthly_dic[i]
         for asin in sub_dic:
-            if asin not in fashion_bad_asin:
+            if asin not in bad_asin:
                 asin_rating_list.append([asin, sub_dic[asin][0] / float(sub_dic[asin][1]), sub_dic[asin][1]])
         asin_rating_list = sorted(asin_rating_list, key=lambda x: x[2])[::-1][:10]
         sub_res = {}
@@ -108,15 +108,30 @@ def top10_most_ratings_data(monthly_dic, meta_data, output_json_path, fashion_ba
 def fashion_data_processing():
     fashion_bad_asin = ['B001XOQTSE', 'B00201ER88', 'B001FOOG0U']
 
-    # meta_data_processing('Fashion/raw_data/meta_AMAZON_FASHION.json.gz', 'Fashion/processed_data/meta_data.json')
+    meta_data_processing('Fashion/raw_data/meta_AMAZON_FASHION.json.gz', 'Fashion/processed_data/meta_data.json')
     processed_meta_data = json.loads(open('Fashion/raw_data/meta_data.json').read())
     ratings_dic = load_ratings_data('Fashion/raw_data/AMAZON_FASHION.csv')
-    top10_avg_ratings_data(ratings_dic, processed_meta_data, 'Fashion/processed_data/top10_avg_ratings.json', fashion_bad_asin)
-    top10_most_ratings_data(ratings_dic, processed_meta_data, 'Fashion/processed_data/top10_most_ratings.json', fashion_bad_asin)
+    top10_avg_ratings_data(ratings_dic, processed_meta_data, 'Fashion/processed_data/top10_avg_ratings.json',
+                           fashion_bad_asin)
+    top10_most_ratings_data(ratings_dic, processed_meta_data, 'Fashion/processed_data/top10_most_ratings.json',
+                            fashion_bad_asin)
+
+
+def all_beauty_data_processing():
+    all_beauty_bad_asin = []
+
+    meta_data_processing('All_Beauty/raw_data/meta_All_Beauty.json.gz', 'All_Beauty/raw_data/meta_data.json')
+    processed_meta_data = json.loads(open('All_Beauty/raw_data/meta_data.json').read())
+    ratings_dic = load_ratings_data('All_Beauty/raw_data/All_Beauty.csv')
+    top10_avg_ratings_data(ratings_dic, processed_meta_data, 'All_Beauty/processed_data/top10_avg_ratings.json',
+                           all_beauty_bad_asin)
+    top10_most_ratings_data(ratings_dic, processed_meta_data, 'All_Beauty/processed_data/top10_most_ratings.json',
+                            all_beauty_bad_asin)
 
 
 def main():
-    fashion_data_processing()
+    # fashion_data_processing()
+    all_beauty_data_processing()
 
 
 if __name__ == "__main__":
